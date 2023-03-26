db1d4f5d2a9fffabf8ded82fc7f1e2a70d48d56c
Bianca Milea <alumil2013@ieselcaminas.org>
2023-03-26 12:42:53 +0200

    9.13 y 9.14 Ioana Bianca Milea

--- src/Futbolista.java
+++ src/Futbolista.java
@@ -1,2 +1,100 @@
-package PACKAGE_NAME;public class Futbolista {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Futbolista implements Comparable <Futbolista> {
+    String dni;
+    String nombre;
+    int edad;
+    int ngoles;
+
+    public Futbolista(String dni, String nombre, int edad, int ngoles) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ngoles = ngoles;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNgoles() {
+        return ngoles;
+    }
+
+    public void setNgoles(int ngoles) {
+        this.ngoles = ngoles;
+    }
+
+    @Override
+    public String toString() {
+        return "Futbolista{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", ngoles=" + ngoles +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Futbolista that = (Futbolista) o;
+        return Objects.equals(dni, that.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni);
+    }
+
+    @Override
+    public int compareTo(Futbolista o) {
+        return this.dni.compareTo(o.getDni());
+    }
+
+    public static Comparator<Futbolista> comparadorPorEdad = new Comparator<Futbolista>() {
+        @Override
+        public int compare(Futbolista f1, Futbolista f2) {
+            return f1.edad - f2.edad;
+        }
+    };
+
+    public static Comparator<Futbolista> comparadorPorNombre = new Comparator<Futbolista>() {
+        @Override
+        public int compare(Futbolista f1, Futbolista f2) {
+            return f1.nombre.compareTo(f2.nombre);
+        }
+    };
+
+    public static Comparator<Futbolista> comparadorPorEdadAndNombre = new Comparator<Futbolista>() {
+        @Override
+        public int compare(Futbolista f1, Futbolista f2) {
+            if (f1.edad == f2.edad) {
+                return f1.nombre.compareTo(f2.nombre);
+            }
+            return f1.edad - f2.edad;
+        }
+    };
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Crear una lista de futbolistas
+        List<Futbolista> futbolistas = new ArrayList<>();
+        futbolistas.add(new Futbolista("9419148G", "Lionel Messi", 34, 710));
+        futbolistas.add(new Futbolista("8759562N", "Cristiano Ronaldo", 37, 780));
+        futbolistas.add(new Futbolista("9568754V", "Neymar Jr", 29, 225));
+        futbolistas.add(new Futbolista("8696965M", "Kylian Mbappé", 21, 88));
+        futbolistas.add(new Futbolista("8659561N", "Erling Haaland", 21, 85));
+
+        // Ordenar la lista por dni
+        Collections.sort(futbolistas);
+
+        System.out.println("Futbolistas ordenados por DNI:");
+        for (Futbolista futbolista : futbolistas) {
+            System.out.println(futbolista.toString());
+        }
+
+        // Ordenar la lista por nombre
+        Collections.sort(futbolistas, Futbolista.comparadorPorNombre);
+
+        System.out.println("\nFutbolistas ordenados por nombre:");
+        for (Futbolista futbolista : futbolistas) {
+            System.out.println(futbolista.toString());
+        }
+
+        // Ordenar la lista por edad
+        Collections.sort(futbolistas, Futbolista.comparadorPorEdad);
+
+        System.out.println("\nFutbolistas ordenados por edad:");
+        for (Futbolista futbolista : futbolistas) {
+            System.out.println(futbolista.toString());
+        }
+
+        // Ordenar la lista por edad y nombre
+        Collections.sort(futbolistas, Futbolista.comparadorPorEdadAndNombre);
+
+        System.out.println("\nFutbolistas ordenados por edad y nombre:");
+        for (Futbolista futbolista : futbolistas) {
+            System.out.println(futbolista.toString());
+        }
+
     }
 }
